1d78507913717280419df5cce1d69a3e2ae8840e
Matsu23 <48169200+Matsu23@users.noreply.github.com>
2022-11-17 17:49:24 -0300

    commit inicial

---
+++ Index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<link rel='stylesheet' href='CSS/normalize.css'>
+<link rel='stylesheet' href='CSS/style.css'>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<meta charset="UTF-8">
+</head>
+<body>
+	<header class='header_orange_theme clearfix'>
+		<img class='header_SmartBrainzicon' src='Media/img/brainZtorm1.png'></img>
+		<h1 class='header_title'>
+			SmartBrainZ
+		</h1>
+		
+		<div class='header_input'>sdsdsd</div>
+</header>
+
+	<main class='main'>
+		<div class='container_studentsbg h500' >
+			<div>
+				<h1 class='main_title'>SmartBrainZ</h1>
+				<p class='white_text centerText'>Suba de Nivel enquanto Aprende!</p>
+				
+			</div>
+			<a class='btn' href='note.php'>Acesse Já!</a>
+		</div>
+		
+		<div class='containerOrange'>
+			<h1 class='container_title'>Por que smartBrainZ?</h1>
+			<div class='card clearfix'>
+				<img src='Media/img/flashcardcolor.png'></img>
+				<p class='white_text font50'>Metodologias inteligentes para seus estudo</p>
+			</div>
+			
+			<div class='card clearfix'>
+				<img src='Media/img/studentsicon.png'></img>
+				<p class='white_text font50'>Se conecte a uma rede com diversos alunos e professores</p>
+			</div>
+			
+			
+			<div class='card clearfix'>
+				<img src='Media/img/GameIcon2.png'></img>
+				<p class='white_text font50'>Estude de maneira gamificada e ganhe recompensas!</p>
+			</div>
+			
+		</div>
+	
+	</main>
+	
+	<footer class='footer'>
+		<a href='#' class='snIcon'><img src='Media/img/twitterIcon.webp'></img></a>
+		<a href='#' class='snIcon'><img src='Media/img/youtubeIcon.webp'></img></a>
+		<a href='#' class='snIcon'><img src='Media/img/facebookicon.png'></img></a>
+		<a href='#' class='snIcon'><img src='Media/img/tiktokIcon.png'></img></a>
+		<a href='#' class='snIcon'><img src='Media/img/Instagram_icon.png.webp'></img></a>
+	</footer>
+
+</body>
+</html>
